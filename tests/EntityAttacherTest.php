40bbfc9052144053d1585b50c48fbfa93116cb3b
David Barratt <david@davidwbarratt.com>
2017-06-25 12:12:10 -0400

    Add test for empty collection relationship

--- tests/EntityAttacherTest.php
+++ tests/EntityAttacherTest.php
@@ -2,6 +2,7 @@
 
 namespace GeoSocio\Tests\EntityAttacher;
 
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use Doctrine\Common\Annotations\Reader;
@@ -162,6 +163,66 @@ class EntityAttacherTest extends TestCase
             ->method('getReflectionProperty')
             ->with('related')
             ->willReturn($property);
+
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects($this->once())
+            ->method('getClassMetadata')
+            ->willReturn($metadata);
+
+        $attach = $this->createMock(Attach::class);
+
+        $reader = $this->createMock(Reader::class);
+        $reader->expects($this->once())
+            ->method('getPropertyAnnotations')
+            ->with($property)
+            ->willReturn([
+                $attach,
+            ]);
+
+        $entityAttacher = new EntityAttacher($em, $reader);
+
+        $attached = $entityAttacher->attach($unattached);
+
+        $this->assertEquals($unattached->id, $attached->id);
+        $this->assertEquals($unattached->related->id, $attached->related->id);
+    }
+
+    public function testAttachEmptyCollectionRelationship()
+    {
+        $related = new \stdClass();
+        $related->id = 321;
+
+        $unattached = new \stdClass();
+        $unattached->id = 123;
+        $unattached->related = $related;
+
+        $property = $this->getMockBuilder(\ReflectionProperty::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $collection = $this->createMock(Collection::class);
+        $collection->expects($this->once())
+            ->method('isEmpty')
+            ->willReturn(true);
+
+        $metadata = $this->getMockBuilder(ClassMetadataInfo::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $metadata->expects($this->once())
+            ->method('getAssociationMappings')
+            ->willReturn([
+                [
+                    'fieldName' => 'related',
+                ],
+            ]);
+        $metadata->expects($this->once())
+            ->method('getReflectionProperty')
+            ->with('related')
+            ->willReturn($property);
+        $metadata->expects($this->once())
+            ->method('getFieldValue')
+            ->with($unattached, 'related')
+            ->willReturn($collection);
 
         $em = $this->createMock(EntityManagerInterface::class);
         $em->expects($this->once())
