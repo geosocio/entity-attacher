97ae61f9624d5874e25448ab86013fa2436c5f43
David Barratt <david@davidwbarratt.com>
2017-06-25 11:47:38 -0400

    Add additional tests

--- tests/EntityAttacherTest.php
+++ tests/EntityAttacherTest.php
@@ -31,8 +31,60 @@ class EntityAttacherTest extends TestCase
         $entityAttacher = new EntityAttacher($em, $reader);
 
         $unattached = new \stdClass;
+        $unattached->id = 123;
         $attached = $entityAttacher->attach($unattached);
 
-        $this->assertEquals($unattached, $attached);
+        $this->assertEquals($unattached->id, $attached->id);
+    }
+
+    public function testAttachArgumentFailure()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $em = $this->createMock(EntityManagerInterface::class);
+        $reader = $this->createMock(Reader::class);
+
+
+        $entityAttacher = new EntityAttacher($em, $reader);
+        $entityAttacher->attach(null);
+    }
+
+    public function testAttachFound()
+    {
+        $id = 123;
+        $ids = [
+            'id' => 123,
+        ];
+
+        $unattached = new \stdClass;
+        $unattached->id = $id;
+
+        $existing = new \stdClass;
+        $existing->id = $id;
+
+        $metadata = $this->getMockBuilder(ClassMetadataInfo::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $metadata->expects($this->once())
+            ->method('getIdentifierValues')
+            ->willReturn($ids);
+
+        $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects($this->once())
+            ->method('getClassMetadata')
+            ->willReturn($metadata);
+        $em->expects($this->once())
+            ->method('find')
+            ->with(\stdClass::class, $ids)
+            ->willReturn($existing);
+
+        $reader = $this->createMock(Reader::class);
+
+
+        $entityAttacher = new EntityAttacher($em, $reader);
+
+        $attached = $entityAttacher->attach($unattached);
+
+        $this->assertSame($existing, $attached);
     }
 }
